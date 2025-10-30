f250ef4e81db3633d472a99833f74d11e6ddd1ee
JosueFelix <josuefelix0717@gmail.com>
2025-10-30 14:14:06 -0300

    Estrutura do Projeto com Classes sem func

---
+++ src/Calculadora.java
@@ -0,0 +1,21 @@
+public class Calculadora {
+
+    public int somar(int a, int b) {
+        return 0;
+    }
+
+    public int subtrair(int a, int b) {
+        return 0;
+    }
+
+    public int multiplicar(int a, int b) {
+        return 0;
+    }
+
+    public double dividir(double a, double b) {
+        if (b == 0) {
+            return Double.NaN;
+        }
+        return 0;
+    }
+}
